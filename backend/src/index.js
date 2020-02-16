879bbe0ccdcdc3caa1991a30bb92602cd453f8f9
Guilherme Vargas Coimbra <guivargas01@gmail.com>
2020-02-16 10:47:02 -0300

    Código Semana OmniStack 10

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket')
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.connect('mongodb+srv://guivargas01:omnistack10@cluster0-qmmq4.mongodb.net/omnistack', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
